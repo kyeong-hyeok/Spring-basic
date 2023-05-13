f4b5a457181f9c58cf5cfc132e0ee707343a1150
kyeong-hyeok <minkh4227@naver.com>
2023-05-13 16:21:11 +0900

    [FEAT] AOP 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLOutput;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,7 +42,6 @@ public class MemberService {
     public List<Member> findMembers() {
         return memberRepository.findAll();
     }
-
     public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
